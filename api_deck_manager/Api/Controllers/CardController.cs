b6269027abb96102dff84b80d2a91a0179245da0
Vincenzofdg <vincenzofdg.dev@gmail.com>
2025-05-06 18:27:08 -0300

    feat: end of the day

--- api_deck_manager/Api/Controllers/CardController.cs
+++ api_deck_manager/Api/Controllers/CardController.cs
@@ -1,4 +1,5 @@
-﻿using api_deck_manager.Shared.DTOs;
+﻿using System.Linq;
+using api_deck_manager.Shared.DTOs;
 using api_deck_manager.Shared.Utils;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,40 +9,61 @@ namespace api_deck_manager.Api.Controllers;
 [Route("[controller]")]
 public class CardController : ControllerBase
 {
+    private List<CardDTO> _mockedList;
     public CardController()
     {
+        _mockedList = new List<CardDTO>
+        {
+            new CardDTO
+            {
+                Id = "060525-lVCUOP-173408",
+                OwnerId = "000000-000000-000000",
+                CustomDeckId = "000000-000000-000000",
+                CollectionId = "000000-000000-000000",
+                Name = "Fireball",
+                Description = "Deals damage to target.",
+                Number = 101,
+                ManaCost = "2R",
+                Label = "Rare",
+                Code = "djjs-4"
+            },
+            new CardDTO
+            {
+                Id = "060525-NymvJD-173745",
+                OwnerId = "000000-000000-000000",
+                CustomDeckId = "000000-000000-000000",
+                CollectionId = "000000-000000-000000",
+                Name = "Healing Light",
+                Description = "Restore health.",
+                Number = 102,
+                ManaCost = "1W",
+                Label = "Common",
+                Code = "djjs-5"
+            }
+        };
     }
+
+    [HttpGet(Name = "GetCard")]
+    public IEnumerable<CardDTO> Get([FromQuery] string skip, [FromQuery] string take)
+    {
+        return _mockedList
+            .Skip(Convert.ToInt32(skip))
+            .Take(Convert.ToInt32(take));
+    }
+
+    [HttpGet("{cardId}", Name = "GetCardById")]
+    public string GetById([FromRoute] string cardId)
+    {
+        return cardId;
+    }
+
 
     [HttpPost(Name = "AddCard")]
     public string CreateCard([FromBody] CardDTO card)
     {
         var cardId = IdGenerator.GenerateUniqueId();
-        return card.Name + " has been saved. " + cardId;
+        return "[" + cardId + "] " + card.Name + " has been saved.";
     }
 
-    //[HttpGet(Name = "GetCard")]
-    //public IEnumerable<CardDTO> Get()
-    //{
-    //    return new List<CardDTO>
-    //    {
-    //        new CardDTO
-    //        {
-    //            ColectionId = 654,
-    //            Name = "Fireball",
-    //            Description = "Deals damage to target.",
-    //            Number = 101,
-    //            ManaCost = "2R",
-    //            Label = "Rare"
-    //        },
-    //        new CardDTO
-    //        {
-    //            ColectionId = 655,
-    //            Name = "Healing Light",
-    //            Description = "Restore health.",
-    //            Number = 102,
-    //            ManaCost = "1W",
-    //            Label = "Common"
-    //        }
-    //    };
-    //}
+    
 }
