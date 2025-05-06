b16c60a59e8b0263318362d8b5a5c1f7e6b9166a
Vincenzofdg <vincenzofdg.dev@gmail.com>
2025-05-06 12:56:30 -0300

    feat: [route] card

---
+++ api_deck_manager/Api/Controllers/CardController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api_deck_manager.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CardController : ControllerBase
+    {
+        public CardController()
+        {
+        }
+
+        [HttpGet(Name = "GetCard")]
+        public string Get()
+        {
+            return "Card Route";
+        }
+    }
+}
--- api_deck_manager/Api/Controllers/TesteController.cs
+++ api_deck_manager/Api/Controllers/TesteController.cs
@@ -4,21 +4,21 @@ namespace api_deck_manager.Api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class testController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<testController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public testController(ILogger<testController> logger)
         {
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet(Name = "GetTest")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
