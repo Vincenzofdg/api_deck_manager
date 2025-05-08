a5bedf4a9402c9d1c5ff569c48ab01642c042197
Vincenzofdg <vincenzofdg.dev@gmail.com>
2025-05-08 18:30:29 -0300

    feat: end of the day

---
+++ Test/CardDTOTest.cs
@@ -0,0 +1,20 @@
+﻿using Api.Shared.DTOs;
+using Api.Shared.Utils;
+
+namespace Test;
+
+public class CardDTOTest
+{
+    [Fact]
+    public void TestingGeneratedId()
+    {
+        //var newId = IdGenerator.GenerateUniqueId();
+        //CardDTO card = new CardDTO()
+        //{
+
+        //    Name = "Test",
+        //};
+
+
+    }
+}
---
+++ Test/Shared/Utils/IdGeneratorTest.cs
@@ -0,0 +1,30 @@
+﻿using Api.Shared.Utils;
+
+namespace Test.Shared.Utils;
+
+public class IdGeneratorTest
+{
+    [Fact]
+    public void CorrectFormat()
+    {
+        string id = IdGenerator.GenerateUniqueId();
+        Assert.Matches(@"^\d{6}-[a-zA-Z0-9]{6}-\d{6}$", id);
+    }
+
+    [Fact]
+    public void UniqueValues()
+    {
+        var id1 = IdGenerator.GenerateUniqueId();
+        var id2 = IdGenerator.GenerateUniqueId();
+
+        Assert.NotEqual(id1, id2);
+    }
+
+    [Fact]
+    public void CorrectLength()
+    {
+        var id = IdGenerator.GenerateUniqueId();
+
+        Assert.Equal(20, id.Length);
+    }
+}
